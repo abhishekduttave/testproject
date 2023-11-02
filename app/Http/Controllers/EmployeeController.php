eb75adba4ba50fdafc76c88e1aff3d0f7c0ec7d8
abhishekduttave <115619076+abhishekduttave@users.noreply.github.com>
2023-11-02 18:20:39 +0530

    upload all changes
    upload all changes

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+use Datatables;
+use DB;
+use DateTime;
+use DateTimeZone;
+use Log;
+use Auth;
+use Illuminate\Http\Request;
+use App\Models\Employee;
+use App\Models\Roles;
+use Illuminate\Support\Facades\Hash;
+
+class EmployeeController extends Controller
+{
+	
+    
+    public function index()
+    {
+		/*
+		try {
+			DB::connection()->getPdo();
+			print_r("Connected successfully to: " . DB::connection()->getDatabaseName());
+		} catch (\Exception $e) {
+			die("Could not connect to the database.  Please check your configuration. Error:" . $e );
+		}
+		*/
+		echo 'Hi';
+    }
+	
+	
+}
